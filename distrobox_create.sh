31bd26aef480f555f813ae5b523ac85588985cb4
rodhfr <souzafrodolfo@gmail.com>
2025-12-28 20:30:21 -0300

    add more programs to distrobox container

--- distrobox_create.sh
+++ distrobox_create.sh
@@ -1,6 +1,6 @@
 # priviledge container
 #distrobox create --root --hostname devbox --name devbox -i archlinux --home /home/rodhfr/Dots/devbox
 distrobox create --hostname devbox --name devbox -i archlinux --home /home/rodhfr/Dots/devbox
-distrobox enter devbox -- sudo pacman -Syu --noconfirm git neovim fish waybar ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-nerd-fonts-symbols-common swaybg tree-sitter-cli fzf
+distrobox enter devbox -- sudo pacman -Syu --noconfirm git neovim fish waybar ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-nerd-fonts-symbols-common swaybg tree-sitter-cli fzf fd
 distrobox enter devbox -- sh /home/rodhfr/Dots/setup_devbox.sh
 distrobox enter devbox
