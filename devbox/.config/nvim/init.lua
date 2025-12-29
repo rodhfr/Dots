f52319f2c48aafe680135b419354b99e2d74d4c3
rodhfr <souzafrodolfo@gmail.com>
2025-12-29 16:51:35 -0300

    some hotkeys nvim

--- devbox/.config/nvim/init.lua
+++ devbox/.config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.keymap.set("n", "gp", "oprint()<Esc>i", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-c>", ":%y+<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-a>", "gg:sleep 100m<CR>vG$", { noremap = true, silent = true })
