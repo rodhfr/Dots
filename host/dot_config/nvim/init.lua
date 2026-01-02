7decf1f82d93ef6fce25b15e68fb527d6eda71e8
rodhfr <souzafrodolfo@gmail.com>
2026-01-01 21:38:18 -0300

    Update dot_config

--- host/dot_config/nvim/init.lua
+++ host/dot_config/nvim/init.lua
@@ -1,2 +1,13 @@
-vim.opt.clipboard = 'unnamedplus'
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
+-- copy everything with ctrl c
+vim.keymap.set("n", "<C-c>", ":%y+<CR>", { noremap = true, silent = true })
+-- select everything with ctrl a
+vim.keymap.set("n", "<C-a>", "gg:sleep 100m<CR>vG$", { noremap = true, silent = true })
+-- simple print()
+vim.keymap.set("n", "gp", "iprint()<Esc>i", { noremap = true, silent = true })
+-- print("\n :")
+vim.keymap.set("n", "go", 'iprint("\\n: ")<Esc>3hi', { noremap = true, silent = true })
+-- Wrap visual selection in print() with "gu"
+vim.keymap.set("v", "gu", [[:s/\%V.*/print(&)/<CR>]], { noremap = true, silent = true })
