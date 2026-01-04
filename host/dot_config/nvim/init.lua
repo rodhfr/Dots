eadde191e9b8f67944f1636b1d74a6b84fdd0e92
rodhfr <souzafrodolfo@gmail.com>
2026-01-04 18:47:15 -0300

    Update dot_config

--- host/dot_config/nvim/init.lua
+++ host/dot_config/nvim/init.lua
@@ -6,9 +6,9 @@ vim.keymap.set("n", "<C-c>", ":%y+<CR>", { noremap = true, silent = true })
 -- select everything with ctrl a
 vim.keymap.set("n", "<C-a>", "gg:sleep 100m<CR>vG$", { noremap = true, silent = true })
 -- simple print()
-vim.keymap.set("n", "gp", "iprint()<Esc>i", { noremap = true, silent = true })
+vim.keymap.set("n", "gp", "oprint()<Esc>i", { noremap = true, silent = true })
 -- print("\n :")
-vim.keymap.set("n", "go", 'iprint("\\n: ")<Esc>3hi', { noremap = true, silent = true })
+vim.keymap.set("n", "go", 'oprint("\\n: ")<Esc>3hi', { noremap = true, silent = true })
 -- Wrap visual selection in print() with "gu"
 vim.keymap.set("v", "gu", [[:s/\%V.*/print(&)/<CR>]], { noremap = true, silent = true })
 -- Saves and exit
