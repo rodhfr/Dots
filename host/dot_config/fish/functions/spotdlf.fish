583be0492724c676c325f6b025e5f2c2ec8ccf87
rodhfr <souzafrodolfo@gmail.com>
2026-01-06 02:41:23 -0300

    Update dot_config

---
+++ host/dot_config/fish/functions/spotdlf.fish
@@ -0,0 +1,26 @@
+function spotdlf
+    set BASE "$HOME/Music"
+    set URL ""
+    set DEST ""
+
+    switch (count $argv)
+        case 1
+            set URL $argv[1]
+            set DEST "$BASE"
+        case 2
+            set DEST "$BASE/$argv[1]"
+            set URL $argv[2]
+        case '*'
+            echo "Usage:"
+            echo "  spotdlf <URL>"
+            echo "  spotdlf <folder> <URL>"
+            return 1
+    end
+
+    # Ensure destination exists
+    if not test -d "$DEST"
+        mkdir -p "$DEST"
+    end
+
+    spotdl --output "$DEST" "$URL"
+end
